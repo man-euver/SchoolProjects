68ca1fec96aeede738215b7559363ca6e926296d
VASCO SANTOS NEVES <man_euver@outlook.com>
2021-10-05 16:28:44 +0100

    Simple exercise

--- SchoolProjects/src/main/java/SchoolProjects/Main.java
+++ SchoolProjects/src/main/java/SchoolProjects/Main.java
@@ -4,6 +4,7 @@
  * and open the template in the editor.
  */
 package SchoolProjects;
+import java.awt.*;
 
 /**
  *
@@ -15,7 +16,10 @@ public class Main {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        // TODO code application logic here 
+        EventQueue.invokeLater(() -> {
+            MainFrame f = new MainFrame();
+        });
     }
     
 }
